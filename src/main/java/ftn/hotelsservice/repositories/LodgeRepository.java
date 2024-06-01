5062c445824aea180767824314132e143ba6f5de
Lazar Pavlović <lp.pavlovic.001@gmail.com>
2024-06-01 11:03:04 +0200

    feat: added create lodge functionality

---
+++ src/main/java/ftn/hotelsservice/repositories/LodgeRepository.java
@@ -0,0 +1,12 @@
+package ftn.hotelsservice.repositories;
+
+import ftn.hotelsservice.domain.entities.Lodge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LodgeRepository extends JpaRepository<Lodge, UUID>, QuerydslPredicateExecutor<Lodge> {
+}
